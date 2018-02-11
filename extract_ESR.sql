e5ebd8110e9ab2cb4d3d0c1ab27119433b171525
quzhouxiachuan <1473230133@qq.com>
2018-02-11 14:47:37 -0600

    Update and rename extract_ESR_CRP_hiCRP.sql to extract_ESR.sql

--- extract_ESR.sql
+++ extract_ESR.sql
@@ -18,20 +18,18 @@ where (event_dsc like '%sedimentation%rate%'
 --select top 10 result_units_dsc from   nmh_cerner_dm.lab_result lab 
 --select count(*) from   #table
 
---select top 10 * from nmh_cerner_dm.lab_result where event_dsc like '%sedimentation%'
+--select top 10 * from nmh_cerner_dm.lab_result where clinical_event_id = '1923529861'
+select * from #table where mrd_pt_id = '5285640'
 
 
-
-select  mrd_pt_id, event_cd, event_dsc, result_val_num, event_start_dt_tm, result_units_dsc, ROW_NUMBER() over (partition by mrd_pt_id order by result_val_num ) as rk 
+select  mrd_pt_id, event_cd, event_dsc, result_val_num, event_start_dt_tm, result_units_dsc, ROW_NUMBER() over (partition by mrd_pt_id order by result_val_num desc) as rk 
 into #lab
 from #table
 where (result_val_num is not null)  and (result_units_dsc is not null) 
 
-select  mrd_pt_id, event_cd, event_dsc, result_val_num, event_start_dt_tm,result_units_dsc, ROW_NUMBER() over (partition by mrd_pt_id order by result_val_num desc) as rk 
+select  mrd_pt_id, event_cd, event_dsc, result_val_num, event_start_dt_tm,result_units_dsc --, ROW_NUMBER() over (partition by mrd_pt_id order by result_val_num desc) as rk 
 into #lab_pk
 from #lab  
 where rk = 1 
-
-select  * from #lab_pk 
 
 
