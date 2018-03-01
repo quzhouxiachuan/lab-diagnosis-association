7e57ddff39fa86e57ec821d3d93e32a2bcb7ebdd
quzhouxiachuan <1473230133@qq.com>
2018-02-28 18:20:35 -0600

    Update diagnosis-crp.sql

--- diagnosis-crp.sql
+++ diagnosis-crp.sql
@@ -91,3 +91,41 @@ into #final_tb
 from #final where rk = 1 
 
 select * from #final_tb 
+
+use EDW
+drop table #table 
+drop table #lab 
+drop table #lab_pk 
+
+select *  
+into #table
+from  nmh_cerner_dm.lab_result lab  
+where (event_dsc like '%reactive%protein' 
+		or lab.event_cd = 39896 )    -- c-reactive protein code 
+	--	or lab.event_cd = 2307403 ) -- c-reactive protein hisens
+	 -- or lab.event_cd = 7460) -- sedimentation rate 
+		and  lab.result_status_cd in ( 9,17)  -- authenticated
+		and lab.order_status_cd = 682 -- order complete
+		and mrd_pt_id is not NULL 
+		and result_val_num is not NULL 
+		and valid_until_dt_tm > GETDATE()
+--select top 10 result_units_dsc from   nmh_cerner_dm.lab_result lab 
+--select count(*) from   #table
+
+
+
+
+select  mrd_pt_id, event_cd, event_dsc, result_val_num, event_start_dt_tm, result_units_dsc, ROW_NUMBER() over (partition by mrd_pt_id order by result_val_num desc) as rk 
+into #lab
+from #table
+where (result_val_num is not null)  and (result_units_dsc is not null) 
+
+select  mrd_pt_id, event_cd, event_dsc, result_val_num, event_start_dt_tm,result_units_dsc --, ROW_NUMBER() over (partition by mrd_pt_id order by result_val_num desc) as rk 
+into #lab_pk
+from #lab  
+where rk = 1 
+
+select distinct final.mrd_pt_id, birth_dts, patient_race_mrd_id, patient_gender_mrd_id 
+from  [EDW_IDS].[edw_ids_ir_dm].[patients] pt  
+right join #final_tb final on pt.mrd_pt_id = final.mrd_pt_id  
+where active_ind = 1 
