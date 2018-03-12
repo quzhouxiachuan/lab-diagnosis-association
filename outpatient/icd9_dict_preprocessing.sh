fce05b5e78d23c71b8df1e95c5e31aae5096bc75
quzhouxiachuan <1473230133@qq.com>
2018-03-12 12:04:00 -0500

    Update icd9_dict_preprocessing.sh

--- outpatient/icd9_dict_preprocessing.sh
+++ outpatient/icd9_dict_preprocessing.sh
@@ -5,5 +5,7 @@ sed -e 's/[ \t]*//' file1 > file3
 
 
 cut -f 1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20 ESR_diagnoses_outpatient03112018.txt > file2 
+ sed 's/\t/,/g' file2> file21
 
 paste -d"\t" file2 file3 > ESR_diagnoses_outpatient03112018_icd.txt
+tr -d '\b\r' < ESR_diagnoses_outpatient03112018_icd.txt > ESR_diagnoses_outpatient03112018_icd2.txt
