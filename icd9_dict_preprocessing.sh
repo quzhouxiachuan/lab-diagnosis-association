95487de1e77f7e3388540685e9489d69ad0a6d1a
quzhouxiachuan <1473230133@qq.com>
2018-04-16 11:44:14 -0500

    Update icd9_dict_preprocessing.sh

--- icd9_dict_preprocessing.sh
+++ icd9_dict_preprocessing.sh
@@ -4,7 +4,7 @@ replace all the , and other special symbols. Then re-save as csv file.
 cut -f 1 icd-hisenCRP.txt > file1
 cut -f 2 icd-hisenCRP.txt > file2 
 sed -i -e 's/\,//g' -e "s/'//g" -e 's/\#//g'  -e 's/\"//g' -e 's/\///g' -e 's/\\//g' -e 's/\|//g' file2 
-sed -e 's/[ \t]*//' file2
+sed -i -e 's/[ \t]*//' file2
 paste -d"|" file1 file2 > icd-hisenCRP-after1.csv
 
 #delete duplicated rows -u: unique, -k sort by #1 field, and compare uniqueness based on #1 field. -t, seperate by |
